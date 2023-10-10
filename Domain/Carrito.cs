7a2a6fac11cd821a3a5a102583b7e72f3b629427
daniel <danie@LAPTOP-DCQ1J05M>
2023-10-10 10:15:27 -0300

    Se agrega visualizacion de imagenes

---
+++ Domain/Carrito.cs
@@ -0,0 +1,18 @@
+﻿using dominio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Domain
+{
+    public class Carrito
+    {
+        public int IdArticulo { get; set; }
+
+        public int Cantidad { get; set; }
+
+        public Articulo articulo { get; set; }
+    }
+}
